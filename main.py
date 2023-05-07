40c6716d5c4f593575e52ce727910d45fedc1afb
tonngw <1336572985@qq.com>
2023-05-07 23:09:45 +0800

    feat: first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from json import dumps
+from requests import post
+from os import environ
+
+
+def main():
+    # get pushplus token
+    token = environ.get('token')
+
+    if not token:
+        # 无token则拦截推送
+        return 'PushPlus: 未配置token，无法进行消息推送。'
+    url = 'http://www.pushplus.plus/send/'
+    headers = {'Content-Type':'application/json'}
+
+    data = {
+        "token": token,
+        "title": "每日新闻推送 bot",
+        "content": "<img src='https://api.03c3.cn/zb' />",
+        "template": "html",
+        "channel": "wechat"
+    }
+    data = dumps(data).encode(encoding='utf-8')
+    rsp = post(url=url, data=data, headers=headers)
+
+
+if __name__ == '__main__':
+    main()
